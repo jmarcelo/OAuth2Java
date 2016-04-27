1e1069fcbc8b661b7a2fa0dab8ac1edd24491e5b
José Marcelo Fascio <jmarcelo.sf@gmail.com>
2016-04-27 16:39:53 -0300

    Testes.

---
+++ src/main/java/br/gov/prodesp/webapp01/Teste1.java
@@ -0,0 +1,98 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.gov.prodesp.webapp01;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.apache.oltu.oauth2.client.request.OAuthClientRequest;
+import org.apache.oltu.oauth2.common.exception.OAuthSystemException;
+import org.apache.oltu.oauth2.common.message.types.ResponseType;
+
+/**
+ *
+ * @author jfascio
+ */
+@WebServlet(name = "Teste1", urlPatterns = {"/Teste1"})
+public class Teste1 extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+        try {
+            OAuthClientRequest oauthReq = OAuthClientRequest
+                    .authorizationLocation("https://login.microsoftonline.com/97ea4bf7-1360-4be4-9925-ae57d82346ef/oauth2/authorize")
+                    .setResponseType("code")
+                    .setClientId("da6a990e-fc37-485d-989a-229a19f80c47")
+                    .setRedirectURI("http://localhost:8080/webapp01-1.0-SNAPSHOT/Redirect")
+                    .buildQueryMessage();
+            
+            
+            Logger.getLogger(Teste1.class.getName()).log(Level.INFO, oauthReq.getLocationUri());
+            
+            response.sendRedirect(oauthReq.getLocationUri());
+            
+        } catch (OAuthSystemException ex) {
+            Logger.getLogger(Teste1.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
